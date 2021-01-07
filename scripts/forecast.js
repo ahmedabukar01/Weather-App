d1907c00b3ec90e6f373bd1ece9656368a07db7f
ahmedabukar01 <ahmedlearn01@gmail.com>
2021-01-07 19:35:34 +0300

    ok

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,4 +1,26 @@
 const key = 'nysrG3uBbXl6PSEXfjhB1DLEGzjrliBo';
-const getLocation = async (city)=>{
-    const query = `?apikey=${key}&q=${city}`;
+// get weather information
+const getWeather = async (id)=>{
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    console.log(data);
 }
+// get city information
+const getCity = async (city)=>{
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+}
+// getCity('hargeisa')
+//  .then(data =>console.log(data))
+//  .catch(err=>console.log(err));
+
+getWeather(301348);
