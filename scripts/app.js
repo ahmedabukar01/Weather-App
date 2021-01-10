21807076836e1d84a38bcdfa40d5d6866023fec7
ahmedabukar01 <ahmedlearn01@gmail.com>
2021-01-10 10:00:05 +0300

    problem solved

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,12 @@
 const form = document.querySelector('form');
 
 let updateUi = async (city) =>{
+
     console.log(city);
 }
 form.addEventListener('submit', e=>{
-    form.preventDefault();
-    const city = form.city.trim();
+    e.preventDefault();
+    const city = form.city.value.trim();
     form.reset();
     updateUi(city);
 
